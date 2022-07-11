4111d347cba021859b245417306d65053ddaeded
“elbub1979” <ilya.leuh09@gmail.com>
2022-07-11 22:32:44 +0300

    added models tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -60,5 +60,52 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
     end
+
+    it 'take the money' do
+      question = game_w_questions.current_game_question
+
+      expect(game_w_questions.status).to eq(:in_progress)
+      expect(game_w_questions.finished?).to be_falsey
+      expect(user.balance).to eq(0)
+
+      3.times do
+        game_w_questions.answer_current_question!(question.correct_answer_key)
+      end
+
+      expect(game_w_questions.take_money!).to be_truthy
+
+      prize = game_w_questions.prize
+      expect(game_w_questions.status).to eq(:money)
+      expect(game_w_questions.finished?).to be_truthy
+      expect(prize).to eq(300)
+      expect(user.balance).to eq(prize)
+    end
+  end
+
+  context '.status' do
+    before(:each) do
+      game_w_questions.finished_at = Time.now
+      expect(game_w_questions.finished?).to be_truthy
+    end
+
+    it ':won' do
+      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+      expect(game_w_questions.status).to eq(:won)
+    end
+
+    it ':fail' do
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:fail)
+    end
+
+    it ':timeout' do
+      game_w_questions.created_at = 1.hour.ago
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:timeout)
+    end
+
+    it ':money' do
+      expect(game_w_questions.status).to eq(:money)
+    end
   end
 end
