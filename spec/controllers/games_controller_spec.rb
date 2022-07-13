edb0bec6fc4f4061eb8feaa2290731655445cecf
“elbub1979” <ilya.leuh09@gmail.com>
2022-07-13 22:56:46 +0300

    added test GamesController#other_actions

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -19,6 +19,9 @@ RSpec.describe GamesController, type: :controller do
 
   # группа тестов для незалогиненного юзера (Анонимус)
   context 'Anon' do
+
+    before { request.env["HTTP_REFERER"] = '/users/sign_in' }
+
     # из экшена show анона посылаем
     it 'kick from #show' do
       # вызываем экшен
@@ -26,6 +29,30 @@ RSpec.describe GamesController, type: :controller do
       # проверяем ответ
       expect(response.status).not_to eq(200) # статус не 200 ОК
       expect(response).to redirect_to(new_user_session_path) # devise должен отправить на логин
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
+    it 'can not create new game' do
+      put :create, headers: { "HTTP_REFERER" => '/users/sign_in' }
+
+      expect(response.status).not_to eq(200) # статус не 200 ОК
+      expect(response).to redirect_to('/users/sign_in')
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
+    it 'can not answer' do
+      put :answer, id: game_w_questions.id, letter: 'a'
+
+      expect(response.status).to eq(302) # статус не 200 ОК
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
+    it 'can not take a money' do
+      put :take_money, id: game_w_questions.id
+
+      expect(response.status).to eq(302) # статус не 200 ОК
+      expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to be # во flash должен быть прописана ошибка
     end
   end
