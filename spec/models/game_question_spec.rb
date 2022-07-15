7848bcff63226106423b40ca72245fcdf7975fd4
“elbub1979” <ilya.leuh09@gmail.com>
2022-07-16 00:07:38 +0300

    refactoring test GamesQuestion

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -10,54 +10,53 @@ RSpec.describe GameQuestion, type: :model do
   # она будет создана на фабрике заново для каждого блока it, где она вызывается
   let(:game_question) { FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
-  # группа тестов на игровое состояние объекта вопроса
-  context 'game status' do
-    # тест на правильную генерацию хэша с вариантами
-    it 'correct .variants' do
+  describe '#variants' do
+    it 'should correct answer variants' do
       expect(game_question.variants).to eq({ 'a' => game_question.question.answer2,
                                              'b' => game_question.question.answer1,
                                              'c' => game_question.question.answer4,
                                              'd' => game_question.question.answer3 })
     end
+  end
 
+  describe '#answer_correct?' do
     it 'correct .answer_correct?' do
-      # именно под буквой b в тесте мы спрятали указатель на верный ответ
       expect(game_question.answer_correct?('b')).to be_truthy
     end
+  end
 
-    it 'correct .level & .text delegates' do
+  describe '#text' do
+    it 'should correct text delegates' do
       expect(game_question.text).to eq(game_question.question.text)
+    end
+  end
+
+  describe 'should correct level delegates' do
+    it 'correct level delegates' do
       expect(game_question.level).to eq(game_question.question.level)
     end
   end
 
-  # help_hash у нас имеет такой формат:
-  # {
-  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
-  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
-  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
-  # }
-  #
-
-  context 'user helpers' do
-    it 'correct audience_help' do
-      expect(game_question.help_hash).not_to include(:audience_help)
-
-      game_question.add_audience_help
-
-      expect(game_question.help_hash).to include(:audience_help)
-
-      ah = game_question.help_hash[:audience_help]
-      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+  describe '#help_hash' do
+    context 'without audience_help hint' do
+      it 'should empty audience_help in help hash' do
+        expect(game_question.help_hash).not_to include(:audience_help)
+      end
+      context 'with audience_help hint' do
+        before { game_question.add_audience_help }
+        let(:ah) { game_question.help_hash[:audience_help] }
+        it 'should help hash included audience_help' do
+          expect(game_question.help_hash).to include(:audience_help)
+        end
+        it 'should all answers keys in the hint' do
+          expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+        end
+      end
     end
   end
 
   describe '#correct_answer_key' do
     it 'correct answer key' do
-      expect(game_question.correct_answer_key).to eq('b')
-    end
-
-    it 'incorrect answer key' do
       expect(game_question.correct_answer_key).to eq('b')
     end
   end
